bc928ee2538fa022d1277ba57764ea413c00f753
Jack Seltmann <33099198+JackSeltmann2020@users.noreply.github.com>
2017-12-27 16:56:49 -0600

    Update SpaceInvaders.java

--- src/spaceinvaders/SpaceInvaders.java
+++ src/spaceinvaders/SpaceInvaders.java
@@ -1,21 +1,239 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spaceinvaders;
 
-/**
- *
- * @author briansanchez
- */
-public class SpaceInvaders {
+import java.io.*; 
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    /**
-     * @param args the command line arguments
-     */
+public class SpaceInvaders extends JFrame implements Runnable {
+    static final int WINDOW_WIDTH = 600;
+    static final int WINDOW_HEIGHT = 800;
+    final int XBORDER = 40;
+    final int YBORDER = 40;
+    final int YTITLE = 25;
+    boolean animateFirstTime = true;
+    int xsize = -1;
+    int ysize = -1;
+    Image image;
+    Graphics2D g;
+
+    
+    
+    static SpaceInvaders frame;
     public static void main(String[] args) {
-        // TODO code application logic here
+        frame = new SpaceInvaders();
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public SpaceInvaders() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.BUTTON1 == e.getButton()) {
+                    
+          
+                   
+                }
+                if (e.BUTTON3 == e.getButton()) {
+                    
+                    reset();
+                }
+                repaint();
+            }
+        });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
+        repaint();
+      }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+
+
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_UP == e.getKeyCode()) {
+                } else if (e.VK_DOWN == e.getKeyCode()) {
+                } else if (e.VK_LEFT == e.getKeyCode()) {
+                } else if (e.VK_RIGHT == e.getKeyCode()) {
+                }
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+    Thread relaxer;
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+
+
+
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || xsize != getSize().width || ysize != getSize().height) {
+            xsize = getSize().width;
+            ysize = getSize().height;
+            image = createImage(xsize, ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, xsize, ysize);
+
+        int x[] = {getX(0), getX(getWidth2()), getX(getWidth2()), getX(0), getX(0)};
+        int y[] = {getY(0), getY(0), getY(getHeight2()), getY(getHeight2()), getY(0)};
+ 
+        g.setColor(Color.black);
+        g.fillPolygon(x, y, 4);
+
+        g.setColor(Color.black);
+        g.drawPolyline(x, y, 5);
+
+        if (animateFirstTime) {
+            gOld.drawImage(image, 0, 0, null);
+            return;
+        }
+ 
+        gOld.drawImage(image, 0, 0, null);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+
+    public void drawAlien(int xpos,int ypos,double rot,
+    double xscale,double yscale,int value)
+    {
+        g.translate(xpos,ypos);
+        g.rotate(rot  * Math.PI/180.0);
+        g.scale( xscale , yscale );
+
+        int xval[] = {10,-10,-10,-5,-10,0,10,5,10};
+        int yval[] = {-20,-20,-5,-5,10,0,10,-5,-5};
+        g.fillPolygon(xval,yval,xval.length);             
+        
+        g.setColor(Color.black);
+        g.setFont(new Font("Arial",Font.PLAIN,16));
+        g.drawString("" + value, 0, 0);        
+                
+        g.scale( 1.0/xscale,1.0/yscale );
+        g.rotate(-rot  * Math.PI/180.0);
+        g.translate(-xpos,-ypos);
+    }          
+////////////////////////////////////////////////////////////////////////////
+    public void drawCannon(int xpos,int ypos,double rot,
+            double xscale,double yscale)
+    {
+        g.translate(xpos,ypos);
+        g.rotate(rot  * Math.PI/180.0);
+        g.scale( xscale , yscale );    
+        
+        int xvals[] = {0,10,10,-10,-10,0};
+        int yvals[] = {-15,-10,10,10,-10,-15};
+        g.fillPolygon(xvals,yvals,xvals.length);
+       
+        g.scale( 1.0/xscale,1.0/yscale );
+        g.rotate(-rot  * Math.PI/180.0);
+        g.translate(-xpos,-ypos);
+    }
+  
+////////////////////////////////////////////////////////////////////////////
+    public void drawCannonBall(int xpos,int ypos,double rot,double xscale,double yscale)
+    {
+        g.translate(xpos,ypos);
+        g.rotate(rot  * Math.PI/180.0);
+        g.scale( xscale , yscale );
+
+      
+        g.fillOval(-10,-10,20,20);
+
+        g.scale( 1.0/xscale,1.0/yscale );
+        g.rotate(-rot  * Math.PI/180.0);
+        g.translate(-xpos,-ypos);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            double seconds = .01;    
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+
+        
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (xsize != getSize().width || ysize != getSize().height) {
+                xsize = getSize().width;
+                ysize = getSize().height;
+            }
+         
+            reset();
+
+        }
+        
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+/////////////////////////////////////////////////////////////////////////
+    public int getX(int x) {
+        return (x + XBORDER);
+    }
+
+    public int getY(int y) {
+        return (y + YBORDER + YTITLE);
+    }
+
+    public int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE + getHeight2());
     }
     
+    
+    public int getWidth2() {
+        return (xsize - getX(0) - XBORDER);
+    }
+
+    public int getHeight2() {
+        return (ysize - getY(0) - YBORDER);
+    }
 }
